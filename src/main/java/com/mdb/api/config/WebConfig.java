af3f1ddd86a3f4ee5b1ea815546f6bbba9d0032f
Ivaylo Sieme <is@niv-ventures.com>
2024-04-02 16:51:08 +0200

    allow any cors

--- src/main/java/com/mdb/api/config/WebConfig.java
+++ src/main/java/com/mdb/api/config/WebConfig.java
@@ -10,10 +10,19 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")  // Apply to all endpoints
+                    .allowedOrigins("*")  // Allow all origins
+                    .allowedMethods("*")  // Allow all methods
+                    .allowedHeaders("*")  // Allow all headers
+                    .allowCredentials(true);  // Allow credentials
+    }
+    /*
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+    */
 }
