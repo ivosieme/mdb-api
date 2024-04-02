6cd5c1a11afccb4a6653afe41de5f2719a7ce2d7
Ivaylo Sieme <is@niv-ventures.com>
2024-04-02 15:12:28 +0200

    test log

--- src/main/java/com/mdb/api/repository/StockSymbolRepository.java
+++ src/main/java/com/mdb/api/repository/StockSymbolRepository.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.mongodb.client.result.UpdateResult;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class StockSymbolRepository{
@@ -57,8 +58,8 @@ public class StockSymbolRepository{
 
     public StockSymbol updateBySymbol(String symbol, StockSymbol stockSymbol) throws BadRequestException {
         System.out.println("repository enter for symbol: " + stockSymbol.getSymbol());
-        if (symbol != stockSymbol.getSymbol()) {
-            System.out.println("Symbol dies not match the Object symbol given: " + stockSymbol.getSymbol() + " Smybol" + symbol);
+        if (!Objects.equals(symbol, stockSymbol.getSymbol())) {
+            System.out.println("Symbol dies not match the Object symbol given: '" + stockSymbol.getSymbol() + "' - Smybol '" + symbol + "'");
             throw new BadRequestException("Symbol dies not match the Object symbol given.");
         }
         System.out.println("performing PERSIST for symbol: " + stockSymbol.getSymbol());
